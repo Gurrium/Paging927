9d053b28d4364cd4d5f4af426d67ab976bec08a6
gurrium <gurrium@hatena.ne.jp>
2021-06-19 17:42:06 +0900

    i

---
+++ Example/Pods/Target Support Files/Paging927/Paging927-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Paging927 : NSObject
+@end
+@implementation PodsDummy_Paging927
+@end
---
+++ Example/Pods/Target Support Files/Pods-Paging927_Example/Pods-Paging927_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Paging927_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Paging927_Example
+@end
